5bfbde2599aef0c35a6da11bc3657c7ff6f48c90
DENCODER <hetcjoshi1684@gmail.com>
2022-07-22 19:13:12 +0530

    face detection and recognition modules added

---
+++ main.py
@@ -0,0 +1,5 @@
+# Date    : 20/07/22 10:01 PM
+# Author  : dencoder (hetcjoshi1684@gmail.com)
+# GitHub    : (https://github.com/D-ENCODER)
+# Twitter    : (https://twitter.com/Hetjoshi1684)
+# Version : 1.0.0
