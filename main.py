cbc133c1586844d86a04304da295d60d9156828e
DENCODER <hetcjoshi1684@gmail.com>
2022-08-09 19:21:19 +0530

    Login Screen Added

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 # Twitter    : (https://twitter.com/Hetjoshi1684)
 # Version : 1.0.0
 
+import math
+import random
+import smtplib
 import customtkinter as ctk
 from Facial_Recognition import generate_dataset, recognize, train_model
 
@@ -37,11 +40,68 @@ def generate():
     app.mainloop()
 
 
-button = ctk.CTkButton(master=app, text="Generate Dataset", command=generate)
-button.place(relx=0.5, rely=0.3, anchor=ctk.CENTER)
-button1 = ctk.CTkButton(master=app, text="Train Model", command=train_model.trainModel)
-button1.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
-button2 = ctk.CTkButton(master=app, text="Face Recognition", command=recognize.recognize)
-button2.place(relx=0.5, rely=0.7, anchor=ctk.CENTER)
+user_email = ctk.CTkLabel(master=app, text="Email")
+user_email.grid(row=1, column=0)
+email = ctk.CTkEntry(master=app, placeholder_text='Enter Email')
+email.grid(row=1, column=2, columnspan=2, pady=20, padx=20)
+# user_id.place()
+user_password = ctk.CTkLabel(master=app, text='Password')
+user_password.grid(row=2, column=0)
+password = ctk.CTkEntry(master=app, placeholder_text='Enter Password')
+password.grid(row=2, column=2, columnspan=2, pady=20, padx=20)
+confirm_password = ctk.CTkLabel(master=app, text='Confirm Password')
+confirm_password.grid(row=3, column=0)
+con_password = ctk.CTkEntry(master=app, placeholder_text='Renter Password')
+con_password.grid(row=3, column=2, columnspan=2, pady=20, padx=20)
+
+
+def sendOtp(emailid):
+    digits = "0123456789"
+    OTP = ""
+    for i in range(6):
+        OTP += digits[math.floor(random.random() * 10)]
+    message = OTP + " use this otp to login"
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+    s.starttls()
+    s.login("facioaide2022@gmail.com", "zajtyiewzpnokcrd")
+    s.sendmail("&&&&&&", emailid, message)
+    return OTP
+
+
+def sendCredentials():
+    emailid = email.get()
+    if password.get() == con_password.get():
+        user_email.destroy()
+        email.destroy()
+        user_password.destroy()
+        password.destroy()
+        confirm_password.destroy()
+        con_password.destroy()
+        submit_button.destroy()
+        otps = sendOtp(emailid)
+        user_otp = ctk.CTkLabel(master=app, text="OTP")
+        user_otp.grid(row=1, column=0)
+        otp = ctk.CTkEntry(master=app, placeholder_text='Enter OTP')
+        otp.grid(row=1, column=2, columnspan=2, pady=20, padx=20)
+
+        def verifyCredentials():
+            if otps == otp.get():
+                user_otp.destroy()
+                otp.destroy()
+                verify.destroy()
+                button = ctk.CTkButton(master=app, text="Generate Dataset", command=generate)
+                button.place(relx=0.5, rely=0.3, anchor=ctk.CENTER)
+                button1 = ctk.CTkButton(master=app, text="Train Model", command=train_model.trainModel)
+                button1.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+                button2 = ctk.CTkButton(master=app, text="Face Recognition", command=recognize.recognize)
+                button2.place(relx=0.5, rely=0.7, anchor=ctk.CENTER)
+
+        verify = ctk.CTkButton(master=app, text='Verify', command=verifyCredentials)
+        verify.grid(row=4, column=1, columnspan=2)
+
+
+submit_button = ctk.CTkButton(master=app, text='Submit', command=sendCredentials)
+submit_button.grid(row=4, column=1, columnspan=2)
+
 
 app.mainloop()
