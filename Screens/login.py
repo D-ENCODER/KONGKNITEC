a1192f9ee19e891164178c0336679113e9cb5de5
DENCODER <hetcjoshi1684@gmail.com>
2022-08-31 20:34:40 +0530

    Login page full screen added and changed according to let's chat app

--- Screens/login.py
+++ Screens/login.py
@@ -7,7 +7,7 @@ import customtkinter as ctk
 from PIL import Image, ImageTk
 from Helper_Functions import otp_sender, custom_error_box
 from Facial_Recognition import generate_dataset, recognize, train_model
-from configure import window_width, window_height
+from configure import *
 
 
 class Login(ctk.CTk):
@@ -16,54 +16,49 @@ class Login(ctk.CTk):
         self.title('Kongknitec')
         self.resizable(False, False)
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.configure(bg=non_dominant_color)
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
-        x_cordinate = int((screen_width / 2) - (window_width / 2))
-        y_cordinate = int((screen_height / 2) - (window_height / 2))
-        self.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+        self.geometry("{}x{}+{}+{}".format(screen_width, screen_height, 0, 0))
+        mainframe = ctk.CTkFrame(master=self, fg_color=non_dominant_color)
         image = Image.open('Icons/logo.png')
-        resized_img = image.resize((90, 90), Image.ANTIALIAS)
-        self.img = ImageTk.PhotoImage(master=self, image=resized_img)
-        logo = ctk.CTkLabel(master=self, image=self.img, anchor='center')
-        # logo.grid(row=0, column=1, columnspan=4,  padx=10)
-        logo.place(relx=0, x=150, y=20, anchor='n')
-        login_label = ctk.CTkLabel(master=self, text='SIGN IN', text_font=("Microsoft YaHei UI Light", 13, "bold"),
-                                   )
-        login_label.place(x=146, relx=0, y=110, anchor='n')
-        login_sublabel = ctk.CTkLabel(master=self, text='USE YOUR KONGKNITEC ACCOUNT',
-                                      text_font=("Microsoft YaHei UI Light", 9, "bold"))
-        login_sublabel.place(x=150, relx=0, y=137, anchor='n')
-        email_frame = ctk.CTkFrame(master=self)
-        email_entry = ctk.CTkEntry(master=email_frame, placeholder_text='E-Mail', fg_color='#212325', border_width=0,
-                                   text_font=("Microsoft YaHei UI Light", 11, 'bold'), width=210, height=30)
-        ctk.CTkFrame(master=self, width=250, height=3).place(x=15, y=205)
-        email_entry.pack(side='left')
-        email_frame.place(x=120, relx=0, y=175, anchor='n')
-        password_frame = ctk.CTkFrame(master=self)
-        password_entry = ctk.CTkEntry(master=password_frame, placeholder_text='Password ', fg_color='#212325', height=30
-                                      , border_width=0, text_font=("Microsoft YaHei UI Light", 11, 'bold'), width=210)
-        ctk.CTkFrame(master=self, width=250, height=3).place(x=15, y=250)
-        password_entry.pack(side='left')
-        password_frame.place(x=118, relx=0, y=220, anchor='n')
+        resized_img = image.resize((120, 120), Image.ANTIALIAS)
+        mainframe.img = ImageTk.PhotoImage(master=mainframe, image=resized_img)
+        ctk.CTkLabel(master=mainframe, image=mainframe.img, anchor='center').grid(row=0, column=0, columnspan=2)
+        ctk.CTkLabel(master=mainframe, text='Welcome to KONGKNITEC', anchor='center',
+                     text_font=(font, 10, "bold"), text_color=dominant_color).grid(row=1, column=0, columnspan=2)
+        ctk.CTkLabel(master=mainframe, text='', anchor='center').grid(row=2, column=0, columnspan=2)
+        ctk.CTkLabel(master=mainframe, text='LOGIN', text_font=(font, 14, "bold"),
+                     anchor='w', text_color=dominant_color).grid(row=3, column=0)
+        email_entry = ctk.CTkEntry(master=mainframe, placeholder_text='Enter your email', fg_color=hyperlink_color,
+                                   border_width=0, corner_radius=10, text_font=(font, 10), width=250, height=35)
+        email_entry.grid(row=4, column=0, columnspan=2, pady=10)
+        password_entry = ctk.CTkEntry(master=mainframe, placeholder_text='Password', fg_color=hyperlink_color,
+                                      height=35, border_width=0, text_font=(font, 10), corner_radius=10, width=250)
+        password_entry.grid(row=5, column=0, columnspan=2, pady=10)
 
         def pressed(string):
             print(string)
 
-        forgot_password = ctk.CTkButton(master=self, text='FORGOT PASSWORD ?', cursor="hand2", fg_color='#212325',
-                                        text_font=("Microsoft YaHei UI Light", 9, "bold"), hover_color='#212325',
-                                        command=lambda: pressed('hello'))
-        forgot_password.place(x=76, relx=0, y=270, anchor='n')
-        some_text = ctk.CTkLabel(master=self, text='SOME TEXT LIKE LEARN MORE AND ETC.',
-                                 text_font=("Microsoft YaHei UI Light", 9, "bold"))
-        some_text.place(x=130, relx=0, y=305, anchor='n')
-        button_frame = ctk.CTkFrame(master=self, fg_color='#212325')
-        create_button = ctk.CTkButton(master=button_frame, text='CREATE ACCOUNT', width=70, height=35,
-                                      text_font=("Microsoft YaHei UI Light", 10, "bold"))
-        create_button.pack(side='left', padx=45)
-        login_button = ctk.CTkButton(master=button_frame, text='LOGIN', width=70, height=35,
-                                     text_font=("Microsoft YaHei UI Light", 10, "bold"))
-        login_button.pack(side='left', padx=30)
-        button_frame.place(x=140, relx=0, y=350, anchor='n')
+        forgot_password = ctk.CTkButton(master=mainframe, text='FORGOT PASSWORD ?', cursor="hand2",
+                                        fg_color=non_dominant_color, text_font=(font, 7, "bold"),
+                                        hover_color=non_dominant_color, command=lambda: pressed('hello'),
+                                        text_color=dominant_color)
+        forgot_password.grid(row=6, column=1, sticky='e')
+        login_button = ctk.CTkButton(master=mainframe, text='LOGIN', width=100, height=35, fg_color=dominant_color,
+                                     text_font=(font, 10, "bold"), hover_color=something, corner_radius=15,
+                                     text_color=hover_color)
+        login_button.grid(row=7, column=0, columnspan=2, pady=10)
+        ctk.CTkLabel(master=mainframe, text='Dont have an account?',
+                     text_font=(font, 9), text_color=hover_color).grid(row=8, column=0, sticky='e')
+        create_button = ctk.CTkButton(master=mainframe, text='Sign-up', width=70, height=35,
+                                      cursor="hand2", fg_color=non_dominant_color, hover_color=non_dominant_color,
+                                      text_color=dominant_color, text_font=(font, 10, "bold"))
+        create_button.grid(row=8, column=1, sticky='w')
+        ctk.CTkLabel(master=mainframe, text='--OR--',
+                     text_font=(font, 9, "bold"), text_color=dominant_color).grid(row=9, column=0, columnspan=2)
+        # TODO: here comes the third party login button.
+        mainframe.grid(row=0, column=0, pady=(screen_height - 500) / 2, padx=(screen_width - 300) / 2)
 
     def on_closing(self):
         self.destroy()
--- configure.py
+++ configure.py
@@ -4,5 +4,9 @@
 # Twitter    : (https://twitter.com/Hetjoshi1684)
 # Version : 1.0.0
 
-window_height = 400
-window_width = 300
+dominant_color = '#1de9b6'
+hyperlink_color = '#212121'
+something = '#3D77BB'
+hover_color = '#ffffff'
+non_dominant_color = '#1a1a1a'
+font = 'Roboto-Regular'
