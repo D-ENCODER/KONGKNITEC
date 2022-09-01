4df309b8ef858c011fdd9f99620dbe2a6290c211
DENCODER <hetcjoshi1684@gmail.com>
2022-09-01 09:26:15 +0530

    Some variable changes made

--- configure.py
+++ configure.py
@@ -6,7 +6,6 @@
 
 dominant_color = '#1de9b6'
 hyperlink_color = '#212121'
-something = '#3D77BB'
-hover_color = '#ffffff'
-non_dominant_color = '#1a1a1a'
+non_dominant_color = '#ffffff'
+hover_color = '#1a1a1a'
 font = 'Roboto-Regular'
