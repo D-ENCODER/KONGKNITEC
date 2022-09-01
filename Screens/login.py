028445edd7a9181101b434c25505171ef897a3bd
DENCODER <hetcjoshi1684@gmail.com>
2022-09-01 09:36:59 +0530

    Some colours were changed

--- Screens/login.py
+++ Screens/login.py
@@ -16,17 +16,19 @@ class Login(ctk.CTk):
         self.title('Kongknitec')
         self.resizable(False, False)
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.configure(bg=non_dominant_color)
+        self.configure(bg=hover_color)
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
         self.geometry("{}x{}+{}+{}".format(screen_width, screen_height, 0, 0))
-        mainframe = ctk.CTkFrame(master=self, fg_color=non_dominant_color)
+        mainframe = ctk.CTkFrame(master=self, fg_color=hover_color)
         image = Image.open('Icons/logo.png')
         resized_img = image.resize((120, 120), Image.ANTIALIAS)
         mainframe.img = ImageTk.PhotoImage(master=mainframe, image=resized_img)
         ctk.CTkLabel(master=mainframe, image=mainframe.img, anchor='center').grid(row=0, column=0, columnspan=2)
-        ctk.CTkLabel(master=mainframe, text='Welcome to KONGKNITEC', anchor='center',
-                     text_font=(font, 10, "bold"), text_color=dominant_color).grid(row=1, column=0, columnspan=2)
+        ctk.CTkLabel(master=mainframe, text='Welcome to', anchor='e',
+                     text_font=(font, 10, "bold"), text_color=non_dominant_color).grid(row=1, column=0)
+        ctk.CTkLabel(master=mainframe, text='KONGKNITEC', anchor='w',
+                     text_font=(font, 10, "bold"), text_color=dominant_color).grid(row=1, column=1)
         ctk.CTkLabel(master=mainframe, text='', anchor='center').grid(row=2, column=0, columnspan=2)
         ctk.CTkLabel(master=mainframe, text='LOGIN', text_font=(font, 14, "bold"),
                      anchor='w', text_color=dominant_color).grid(row=3, column=0)
@@ -41,18 +43,18 @@ class Login(ctk.CTk):
             print(string)
 
         forgot_password = ctk.CTkButton(master=mainframe, text='FORGOT PASSWORD ?', cursor="hand2",
-                                        fg_color=non_dominant_color, text_font=(font, 7, "bold"),
-                                        hover_color=non_dominant_color, command=lambda: pressed('hello'),
+                                        fg_color=hover_color, text_font=(font, 7, "bold"),
+                                        hover_color=hover_color, command=lambda: pressed('hello'),
                                         text_color=dominant_color)
         forgot_password.grid(row=6, column=1, sticky='e')
         login_button = ctk.CTkButton(master=mainframe, text='LOGIN', width=100, height=35, fg_color=dominant_color,
-                                     text_font=(font, 10, "bold"), hover_color=something, corner_radius=15,
+                                     text_font=(font, 10, "bold"), hover_color=dominant_color, corner_radius=15,
                                      text_color=hover_color, command=lambda: pressed("Welcome to Kongknitech \nYou're successfully Log in."))
         login_button.grid(row=7, column=0, columnspan=2, pady=10)
         ctk.CTkLabel(master=mainframe, text='Dont have an account?',
-                     text_font=(font, 9), text_color=hover_color).grid(row=8, column=0, sticky='e')
+                     text_font=(font, 9), text_color=non_dominant_color).grid(row=8, column=0, sticky='e')
         create_button = ctk.CTkButton(master=mainframe, text='Sign-up', width=70, height=35,
-                                      cursor="hand2", fg_color=non_dominant_color, hover_color=non_dominant_color,
+                                      cursor="hand2", fg_color=hover_color, hover_color=hover_color,
                                       text_color=dominant_color, text_font=(font, 10, "bold"))
         create_button.grid(row=8, column=1, sticky='w')
         ctk.CTkLabel(master=mainframe, text='--OR--',
