0f72539a2f076b407e762a17c7f9f0d4fd683dc6
Maulikatgit <mm.2004.parmar@gmail.com>
2022-09-09 18:26:22 -0400

    Third party login icon added.

--- Screens/login.py
+++ Screens/login.py
@@ -27,12 +27,12 @@ class Login(ctk.CTk):
         mainframe = ctk.CTkFrame(master=self, fg_color=hover_color)
         mainframe.img = load_image(mainframe, 'Icons/logo.png', 120)
         ctk.CTkLabel(master=mainframe, image=mainframe.img, anchor='center').grid(row=0, column=0, columnspan=2)
-        ctk.CTkLabel(master=mainframe, text='Welcome to', anchor='e',
+        ctk.CTkLabel(master=mainframe, text='Welcome to ', anchor='e',
                      text_font=(font, 10, "bold"), text_color=non_dominant_color).grid(row=1, column=0)
         ctk.CTkLabel(master=mainframe, text='KONGKNITEC', anchor='w',
                      text_font=(font, 10, "bold"), text_color=dominant_color).grid(row=1, column=1)
         ctk.CTkLabel(master=mainframe, text='', anchor='center').grid(row=2, column=0, columnspan=2)
-        ctk.CTkLabel(master=mainframe, text='LOGIN', text_font=(font, 14, "bold"),
+        ctk.CTkLabel(master=mainframe, text='LOGIN', text_font=(font, 20, "bold"),
                      anchor='w', text_color=dominant_color).grid(row=3, column=0)
         email_entry = ctk.CTkEntry(master=mainframe, placeholder_text='Enter your email', fg_color=hyperlink_color,
                                    border_width=0, corner_radius=10, text_font=(font, 10), width=250, height=35)
@@ -40,6 +40,11 @@ class Login(ctk.CTk):
         password_entry = ctk.CTkEntry(master=mainframe, placeholder_text='Password', fg_color=hyperlink_color,
                                       height=35, border_width=0, text_font=(font, 10), corner_radius=10, width=250)
         password_entry.grid(row=5, column=0, columnspan=2, pady=10)
+
+        photo_frame = ctk.CTkFrame(master=self, fg_color=hyperlink_color)
+        self.twitter_icon = load_image(photo_frame, "Icons/twitter.png", 27)
+        ctk.CTkButton(master=photo_frame, image=self.twitter_icon, text="", width=30, fg_color=hyperlink_color,
+                      hover_color=hyperlink_color, cursor='hand2').grid(row=0, column=2, padx=7, pady=10)
 
         def pressed(string):
             print(string)
@@ -54,7 +59,7 @@ class Login(ctk.CTk):
                                                                       "successfully Log in.")).grid(row=7, column=0,
                                                                                                     columnspan=2,
                                                                                                     pady=10)
-        ctk.CTkLabel(master=mainframe, text='Dont have an account?',
+        ctk.CTkLabel(master=mainframe, text="Don't have an account? ",
                      text_font=(font, 9), text_color=non_dominant_color).grid(row=8, column=0, sticky='e')
         ctk.CTkButton(master=mainframe, text='Sign-up', width=70, height=35,
                       cursor="hand2", fg_color=hover_color, hover_color=hover_color,
@@ -70,8 +75,18 @@ class Login(ctk.CTk):
         self.google_icon = load_image(login_frame, "Icons/google.png", 27)
         ctk.CTkButton(master=login_frame, image=self.google_icon, text="", width=30, fg_color=hyperlink_color,
                       hover_color=hyperlink_color, cursor='hand2',
-                      command=caller).grid(row=0, column=0, padx=7, pady=7)
-        login_frame.grid(row=10, column=0)
+                      command=caller, ).grid(row=0, column=0, padx=7, pady=10)
+
+        self.git_icon = load_image(login_frame, "Icons/git.png", 27)
+        ctk.CTkButton(master=login_frame, image=self.git_icon, text="", width=30, fg_color=hyperlink_color,
+                      hover_color=hyperlink_color, cursor='hand2',
+                      command=caller).grid(row=0, column=1, padx=50, pady=10)
+
+        self.twitter_icon = load_image(login_frame, "Icons/twitter.png", 27)
+        ctk.CTkButton(master=login_frame, image=self.twitter_icon, text="", width=30, fg_color=hyperlink_color,
+                      hover_color=hyperlink_color, cursor='hand2',
+                      command=caller).grid(row=0, column=2, padx=7, pady=10)
+        login_frame.grid(row=10, column=0, columnspan=2)
         # TODO: here comes the third party login button.
         mainframe.grid(row=0, column=0, pady=(screen_height - 500) / 2, padx=(screen_width - 300) / 2)
 
